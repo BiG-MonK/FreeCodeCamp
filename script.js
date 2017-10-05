38d9f4d03952c9b25d69eb365f49fdc696249c11
Sergey Sukharev <s-monk-s@mail.ru>
2017-10-06 00:40:51 +0700

    Найден сервер API

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-$.getJSON("https://api.coinmarketcap.com/v1/ticker/?limit=10", function(json) {
+$.getJSON("https://talaikis.com/api/quotes/random/", function(json) {
 	//var myStr = JSON.stringify(json);
   //myStr = JSON.parse(myStr);
   var html = "";
   //$("#test").html(JSON.stringify(json));
-  console.log(json[1].price_usd);
+  console.log(json);
   json.forEach(function(val) {             //перебор каждого элемента в json
     var keys = Object.keys(val);
     html += "<div class = 'cat'>";
